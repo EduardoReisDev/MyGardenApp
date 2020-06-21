83ea0957d8f3cc8ca5e99510d43a437abf185383
Eduardo Reis <eduardoreisdev@gmail.com>
2020-06-20 23:26:11 -0300

    second

--- MyGarden/MyGarden.Android/Resources/Resource.designer.cs
+++ MyGarden/MyGarden.Android/Resources/Resource.designer.cs
@@ -8375,13 +8375,16 @@ namespace MyGarden.Droid
 			public const int notify_panel_notification_icon_bg = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int splash_screen = 2131165302;
+			public const int pacova = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_dark = 2131165303;
+			public const int splash_screen = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int tooltip_frame_light = 2131165304;
+			public const int tooltip_frame_dark = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int tooltip_frame_light = 2131165305;
 			
 			static Drawable()
 			{
