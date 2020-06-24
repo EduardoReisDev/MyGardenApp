9d92d0c586f85c63352ae9ce58cd3184abfdf5d0
Eduardo Reis <eduardoreisdev@gmail.com>
2020-06-24 19:27:08 -0300

    Detalhe e Cadastro

---
+++ MyGarden/MyGarden.Android/XDateRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using MyGarden.Controls;
+using XamarinBorderlessEntry.Droid.ControlHelpers;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+
+[assembly: ExportRenderer(typeof(XDate), typeof(XDateRenderer))]
+namespace XamarinBorderlessEntry.Droid.ControlHelpers
+{
+    public class XDateRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
