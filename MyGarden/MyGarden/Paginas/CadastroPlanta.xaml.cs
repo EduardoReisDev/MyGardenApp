cf881b0ce6bd6abf7bffc91b72ea3e490d5b7550
Eduardo Reis <eduardoreisdev@gmail.com>
2020-06-26 00:07:56 -0300

    Cadastro

--- MyGarden/MyGarden/Paginas/CadastroPlanta.xaml.cs
+++ MyGarden/MyGarden/Paginas/CadastroPlanta.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MyGarden.Models;
+using MyGarden.Banco;
 
 namespace MyGarden.Paginas
 {
@@ -17,6 +19,26 @@ namespace MyGarden.Paginas
             InitializeComponent();
         }
 
-        public 
+        public async void GoHome(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Home());
+        }
+
+        private void SalvarAction(object sender, EventArgs args)
+        {
+            //Obter dados da tela
+            Planta planta = new Planta();
+            planta.NomePopular = NomePopular.Text;
+            planta.NomeCientifico = NomeCientifico.Text;
+            planta.Observacoes = Observacoes.Text;
+
+            //Salvar informacoes no banco
+            Database database = new Database();
+            database.Cadastro(planta);
+
+            //Voltar para tela de pesquisa
+            App.Current.MainPage = new NavigationPage(new Home());
+
+        }
     }
 }
