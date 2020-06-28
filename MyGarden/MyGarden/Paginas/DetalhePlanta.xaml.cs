7d2c84b2f1f8e96114ea5d6600c64b0d00a1ed9f
Eduardo Reis <eduardoreisdev@gmail.com>
2020-06-27 23:22:27 -0300

    Detalhe e home

--- MyGarden/MyGarden/Paginas/DetalhePlanta.xaml.cs
+++ MyGarden/MyGarden/Paginas/DetalhePlanta.xaml.cs
@@ -6,20 +6,38 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MyGarden.Models;
+using MyGarden.Banco;
 
 namespace MyGarden.Paginas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalhePlanta : ContentPage
     {
-        public DetalhePlanta()
+        public DetalhePlanta(Planta planta)
         {
             InitializeComponent();
+
+            BindingContext = planta;
         }
 
-        public async void GoEditar(object sender, EventArgs args)
+        public void GoEditar(object sender, EventArgs args)
         {
-            await Navigation.PushAsync(new Paginas.EditarPlanta());
+            Button GoEditar = (Button)sender;
+            TapGestureRecognizer tapGest = (TapGestureRecognizer)GoEditar.GestureRecognizers[0];
+            Planta planta = tapGest.CommandParameter as Planta;
+            Navigation.PushAsync(new Paginas.EditarPlanta(planta));
+        }
+
+        public void ExcluirAction(object sender, EventArgs args)
+        {
+            Button ExcluirAction = (Button)sender;
+            TapGestureRecognizer tapGest = (TapGestureRecognizer)ExcluirAction.GestureRecognizers[0];
+            Planta planta = tapGest.CommandParameter as Planta;
+            Database database = new Database();
+            database.Exclusao(planta);
+
+            Navigation.PushAsync(new Home());        
         }
     }
 }
--- MyGarden/MyGarden/Paginas/Home.xaml.cs
+++ MyGarden/MyGarden/Paginas/Home.xaml.cs
@@ -6,19 +6,28 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MyGarden.Banco;
+using MyGarden.Models;
 
 namespace MyGarden.Paginas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+        List<Planta> Lista { get; set; }
+
         public Home()
         {
             InitializeComponent();
+            Database database = new Database();
+            Lista = database.Consultar();
+            ListaPlantas.ItemsSource = database.Consultar();
+            lblCount.Text = Lista.Count.ToString();
         }
 
         public async void GoDetalhe(object sender, EventArgs args)
         {
+            Frame 
             await Navigation.PushAsync(new DetalhePlanta());
         }
 
