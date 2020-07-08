cea5fd89b2074ad6ffd2d090478df7164a1e896a
Eduardo Reis <eduardoreisdev@gmail.com>
2020-07-08 20:21:39 -0300

    IsVisible days

--- MyGarden/MyGarden/Paginas/DetalhePlanta.xaml.cs
+++ MyGarden/MyGarden/Paginas/DetalhePlanta.xaml.cs
@@ -18,7 +18,105 @@ namespace MyGarden.Paginas
         public DetalhePlanta(Planta planta)
         {
             InitializeComponent();
+            VerificaDiaUm(planta);
+            VerificaDiaDois(planta);
+            VerificaDiaTres(planta);
+            VerificaDiaQuatro(planta);
+            VerificaDiaCinco(planta);
+            VerificaDiaSeis(planta);
+            VerificaDiaSete(planta);
             BindingContext = planta;
+        }
+
+        public void VerificaDiaUm(Planta planta)
+        {
+            if (planta.DiaUm == null)
+            {
+                DiaUm.IsVisible = false;
+            }
+
+            else
+            {
+                DiaUm.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaDois(Planta planta)
+        {
+            if (planta.DiaDois == null)
+            {
+                DiaDois.IsVisible = false;
+            }
+
+            else
+            {
+                DiaDois.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaTres(Planta planta)
+        {
+            if (planta.DiaTres == null)
+            {
+                DiaTres.IsVisible = false;
+            }
+
+            else
+            {
+                DiaTres.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaQuatro(Planta planta)
+        {
+            if (planta.DiaQuatro == null)
+            {
+                DiaQuatro.IsVisible = false;
+            }
+
+            else
+            {
+                DiaQuatro.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaCinco(Planta planta)
+        {
+            if (planta.DiaCinco == null)
+            {
+                DiaCinco.IsVisible = false;
+            }
+
+            else
+            {
+                DiaCinco.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaSeis(Planta planta)
+        {
+            if (planta.DiaSeis == null)
+            {
+                DiaSeis.IsVisible = false;
+            }
+
+            else
+            {
+                DiaSeis.IsVisible = true;
+            }
+        }
+
+        public void VerificaDiaSete(Planta planta)
+        {
+            if (planta.DiaSete == null)
+            {
+                DiaSete.IsVisible = false;
+            }
+
+            else
+            {
+                DiaSete.IsVisible = true;
+            }
         }
     }
 }
